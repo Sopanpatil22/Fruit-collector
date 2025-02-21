d41748e6d185ed59dcaf05827719750ba47b51ae
Swarup Sharad Gite <swarupgite75@gmail.com>
2025-02-21 16:08:22 +0530

    add pic

--- Assets/Main_Menu.cs
+++ Assets/Main_Menu.cs
@@ -6,10 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+    private bool isPaused = false;
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(0);
     }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0f : 1f;  // Pause (0) / Resume (1)
+    }
+
 
     public void ExitGame()
     {
